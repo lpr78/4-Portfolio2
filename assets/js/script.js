c4a293059f081ff9c69ccfd9562174c3bc760bcd
lpr78 <lukeprice@blueyonder.co.uk>
2021-10-07 16:37:10 +0000

    basic game element and json file with questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,130 @@
+//Code element for the game.html page
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
 
+let questions = [];
 
-function fn_instructions() {
-  //Getting elements modal elements
-  let modal = document.getElementById("myModal");
-  let btn = document.getElementById("btn_Instruction");
-  let span = document.getElementsByClassName("close")[0];
-  let menu = document.getElementsByClassName("main_menu")[0];
-  let write = document.getElementById("modal_additions");
-  let name = document.getElementById('enter_name').value;
+fetch('assets/js/questions.json')
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions;
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
+//CONSTANTS
+const CORRECT_BONUS = 1;
+const MAX_QUESTIONS = 20;
 
-  // When the user clicks the button, open the modal 
-  btn.onclick = function () {
-    modal.style.display = "block";
-    menu.style.display = "none";
-    let player = name;
-    write.innerHTML = `<p>Welcome ${player} to the binary game!</p>`;
-  }
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
+    getNewQuestion();
+};
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    modal.style.display = "none";
-    menu.style.display = "flex";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-      menu.style.display = "flex";
-
+getNewQuestion = () => {
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+        //go to the end page
+        return window.location.assign('/end.html');
     }
-  }
-}
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
+
+incrementScore = (num) => {
+    score += num;
+    scoreText.innerText = score;
+};
+
+//End page JS Script
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+
+const finalScore = document.getElementById("finalScore");
+
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+finalScore.innerText = mostRecentScore;
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORE = 5;
+
+username.addEventListener("keyup", () => {
+  saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+  e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5); //Keeps maximum splice 
+  localStorage.setItem('highScores',JSON.stringify(highScores));
+  window.location.assign('/index.html');
+};
+
+//Highscore page JS
+const highScoresList = document.getElementById('highScoresList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+highScoresList.innerHTML = highScores.map(score => {
+    return `<li class = "high-score">${score.name}:&emsp;${score.score}</li>`;
+}).join("");
+
+
