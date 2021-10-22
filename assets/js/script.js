8601cc5faa2a54eea2c9b1f5dd93923dc0d3bf69
lpr78 <lukeprice@blueyonder.co.uk>
2021-10-22 15:38:29 +0000

    i added further updates to the constant values after testing the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,36 @@
 //GAME PAGE JS SCRIPT
 //Reference for learning and walkthrough production: https://www.youtube.com/playlist?list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx
-
-//variable values for the game element
-var question = document.getElementById('question');
-var choices = Array.from(document.getElementsByClassName('choice-text'));
-var progressText = document.getElementById('progressText');
-var scoreText = document.getElementById('score');
-var progressBarFull = document.getElementById('progressBarFull');
+//variable and constant values for the game element
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
 var currentQuestion = {};
 var acceptingAnswers = false;
 var score = 0;
 var questionCounter = 0;
 var availableQuesions = []; //initial blank array
 var questions = []; //initial blank array
-
 //retrieving questions - credit to James Q on README file
 fetch("assets/js/questions.json")
     .then(function (res) {
-        return res.json();
-    })
+    return res.json();
+})
     .then(function (loadedQuestions) {
-        questions = loadedQuestions;
-        startGame();
-    })
+    questions = loadedQuestions;
+    startGame();
+})
     .catch(function (err) {
-        console.error(err);
-    });
-
+    console.error(err);
+});
 //CONSTANTS
-var CORRECT_BONUS = 2; //Setting value to 2 - 1 for answer and 1 for workings
-var MAX_QUESTIONS = 20; //max questions from JSON file - can be changed and extended
-
+const CORRECT_BONUS = 2; //Setting value to 2 - 1 for answer and 1 for workings
+const MAX_QUESTIONS = 20; //max questions from JSON file - can be changed and extended
 //Start of the game retrieving elements and setting values
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+const __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++)
+        s += arguments[i].length;
     for (var r = Array(s), k = 0, i = 0; i < il; i++)
         for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
             r[k] = a[j];
@@ -67,7 +64,6 @@ getNewQuestion = function () {
     availableQuesions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
-
 //Updating question choices
 choices.forEach(function (choice) {
     choice.addEventListener('click', function (e) {
@@ -88,15 +84,10 @@ choices.forEach(function (choice) {
         }, 1000);
     });
 });
-
-
 incrementScore = function (num) {
     score += num;
     scoreText.innerText = score;
 };
-
-
-
 //HIGH SCORE HTML PAGE JS CODE
 var highScoresList = document.getElementById('highScoresList');
 var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
